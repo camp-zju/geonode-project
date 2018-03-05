5ec139a2b3291aa5cfaedb24f12a9194c88943f8
Niran814804102 <814804102@qq.com>
2018-03-05 15:12:21 +0800

    certification模块文件首次上传
    完全从worldmap中复制

---
+++ project_name/context_processors.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+def worldmap(request):
+    """
+    Global values specific to WorldMap to pass to templates
+    """
+
+    return {
+        'GEONODE_CLIENT_LOCATION': settings.GEONODE_CLIENT_LOCATION,
+        'DB_DATASTORE': settings.DB_DATASTORE,
+        'HYPERMAP_REGISTRY_URL': settings.HYPERMAP_REGISTRY_URL,
+        'MAPPROXY_URL': settings.MAPPROXY_URL,
+        'SOLR_URL': settings.SOLR_URL,
+        'USE_GAZETTEER': settings.USE_GAZETTEER,
+    }
